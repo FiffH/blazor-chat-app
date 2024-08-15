55bcff279b741d45254eb97cef5e783d64cafc1d
Christopher Harris <cs.harris@outlook.com>
2024-08-16 00:01:08 +0100

    set up shared project for models.

---
+++ Cleo/Cleo.Shared/Models/User.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Cleo.Shared.Models
+{
+    public class User
+    {
+        public string Username { get; set; }
+        public string Role { get; set; }
+    }
+}
